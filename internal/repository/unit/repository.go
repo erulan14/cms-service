106dff19c9b9b540266e7a61b167409dc4897c96
erulan14 <erulan14@yandex.ru>
2024-05-02 11:35:28 +0500

    add unit model

--- internal/repository/unit/repository.go
+++ internal/repository/unit/repository.go
@@ -1,4 +1,4 @@
-package unit
+package geozone
 
 import (
 	"cms-service/internal/model"
@@ -18,48 +18,37 @@ func NewRepository(conn *pgxpool.Pool) def.UnitRepository {
 	return &repository{Conn: conn}
 }
 
-func (r *repository) Create(ctx context.Context, deviceUUID string, model *model.CreateUnitDTO) error {
-	_, err := r.Conn.Exec(ctx, `INSERT INTO 
-    device (uuid, name, hw, phone, phone2, 
-            tariff, icon, creator, sensors, ) VALUES ($1, $2, $3, $4)`,
-		deviceUUID, model.Name, model.Company, model.Port)
-	if err != nil {
-		return err
-	}
+func (r *repository) Create(ctx context.Context, geozoneUUID string, model *model.CreateUnitDTO) error {
+
 	return nil
 }
 
 func (r *repository) List(ctx context.Context) ([]model.Unit, error) {
-	rows, _ := r.Conn.Query(ctx, `SELECT * FROM device`)
-	devices, err := pgx.CollectRows(rows, pgx.RowToStructByPos[model.Unit])
+	rows, _ := r.Conn.Query(ctx, `SELECT * FROM geozone`)
+	geozones, err := pgx.CollectRows(rows, pgx.RowToStructByPos[model.Unit])
 	if err != nil {
 		return nil, err
 	}
-	return devices, nil
+	return geozones, nil
 }
 
-func (r *repository) Get(ctx context.Context, deviceUUID string) (*model.Unit, error) {
-	row, _ := r.Conn.Query(ctx, `SELECT * FROM device WHERE uuid = $1`, deviceUUID)
-	device, err := pgx.CollectOneRow(row, pgx.RowToStructByPos[model.Unit])
+func (r *repository) Get(ctx context.Context, geozoneUUID string) (*model.Unit, error) {
+	row, _ := r.Conn.Query(ctx, `SELECT * FROM geozone WHERE uuid = $1`, geozoneUUID)
+	geozone, err := pgx.CollectOneRow(row, pgx.RowToStructByPos[model.Unit])
 
 	if err != nil {
 		return nil, err
 	}
-	return &device, nil
+	return &geozone, nil
 }
 
-func (r *repository) Update(ctx context.Context, deviceUUID string, model *model.UpdateUnitDTO) error {
-	_, err := r.Conn.Exec(ctx, `UPDATE device SET name = $2, company = $3, port = $4 WHERE uuid = $1`,
-		deviceUUID, model.Name, model.Company, model.Port)
-	if err != nil {
-		return err
-	}
+func (r *repository) Update(ctx context.Context, geozoneUUID string, model *model.UpdateUnitDTO) error {
 
 	return nil
 }
 
-func (r *repository) Delete(ctx context.Context, deviceUUID string) error {
-	_, err := r.Conn.Exec(ctx, `DELETE FROM device WHERE uuid = $1`, deviceUUID)
+func (r *repository) Delete(ctx context.Context, geozoneUUID string) error {
+	_, err := r.Conn.Exec(ctx, `DELETE FROM geozone WHERE uuid = $1`, geozoneUUID)
 	if err != nil {
 		return err
 	}
